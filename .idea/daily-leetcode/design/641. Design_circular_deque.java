d3b8cab382c6f7080735da05678fd055b268adce
Bhargav <myapple@Mys-Laptop.local>
2024-09-28 12:03:05 +0530

    28-09-2024

--- .idea/daily-leetcode/design/641. Design_circular_deque.java
+++ .idea/daily-leetcode/design/641. Design_circular_deque.java
@@ -1,5 +1,6 @@
 /**
  * https://leetcode.com/problems/design-circular-deque/description/
+ * Using DLL
  */
 class MyCircularDeque {
     class DLL {
@@ -105,6 +106,109 @@ class MyCircularDeque {
 
     public boolean isFull() {
         return size == capacity;
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
+
+
+/**
+ * Using Array
+ */
+class MyCircularDeque {
+    int[] deque;
+    int front, rear, currSize, size;
+
+    public MyCircularDeque(int k) {
+        deque = new int[k];
+        front = rear = -1;
+        currSize = 0;
+        size = k;
+    }
+
+    public boolean insertFront(int value) {
+        if (isFull()) return false;
+        else {
+            if (isEmpty()) {
+                front = rear = 0;
+                deque[front] = value;
+            } else {
+                front = (front-1+size)%size;
+                deque[front] = value;
+            }
+            currSize++;
+            return true;
+        }
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) return false;
+        else {
+            if (isEmpty()) {
+                front = rear = 0;
+                deque[rear] = value;
+            } else {
+                rear = (rear+1)%size;
+                deque[rear] = value;
+            }
+            currSize++;
+            return true;
+        }
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) return false;
+        else {
+            if (currSize == 1) {
+                front = rear = -1;
+            } else {
+                front = (front+1)%size;
+            }
+            currSize--;
+            return true;
+        }
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) return false;
+        else {
+            if (currSize == 1) {
+                front = rear = -1;
+            } else {
+                rear = (rear-1+size)%size;
+            }
+            currSize--;
+            return true;
+        }
+    }
+
+    public int getFront() {
+        if (isEmpty()) return -1;
+        return deque[front];
+    }
+
+    public int getRear() {
+        if (isEmpty()) return -1;
+        return deque[rear];
+    }
+
+    public boolean isEmpty() {
+        return currSize == 0;
+    }
+
+    public boolean isFull() {
+        return currSize == size;
     }
 }
 
